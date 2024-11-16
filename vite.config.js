67fff23b1261c14d9d5fdff477b6074586bcbf56
Nalhab <nathanlrbn@gmail.com>
2024-11-16 19:10:38 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -5,48 +5,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: "/",
-    clearScreen: false,
-    optimizeDeps: {
-        esbuildOptions: {
-            supported: {
-                'top-level-await': true
-            }
-        },
-        include: ['react', 'react-dom']
-    },
-    esbuild: {
-        supported: {
-            'top-level-await': true
-        }
-    },
-    build: {
-        sourcemap: true,
-        rollupOptions: {
-            external: ['react', 'react-dom', 'react/jsx-runtime'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                    'react/jsx-runtime': 'jsxRuntime'
-                }
-            }
-        }
-    },
-    server: {
-        open: true
-    },
     plugins: [
         react(),
         viteStaticCopy({
             targets: [
                 { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
                 { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' },
-                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.js', dest: 'jsm/libs/draco/gltf' },
-                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.wasm', dest: 'jsm/libs/draco/gltf/' },
-                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_encoder.js', dest: 'jsm/libs/draco/gltf/' },
-                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
+                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/*', dest: 'jsm/libs/draco/gltf/' }
             ]
         }),
         glsl()
-    ]
-})
+    ],
+    server: {
+        open: true
+    },
+    optimizeDeps: {
+        include: ['react', 'react-dom']
+    },
+    build: {
+        sourcemap: true
+    }
+});
