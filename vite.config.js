ebadbccf21234b6693b5032476b528c0da41fdd8
Nalhab <nathanlrbn@gmail.com>
2024-11-16 20:28:23 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         include: [
             'three',
             'tslib',
+            '@emotion/is-prop-valid',
             '@fortawesome/fontawesome-svg-core',
             '@fortawesome/free-solid-svg-icons',
             '@fortawesome/react-fontawesome',
@@ -61,6 +62,7 @@ export default defineConfig({
         alias: {
             'three': 'three',
             'tslib': 'tslib',
+            '@emotion/is-prop-valid': '@emotion/is-prop-valid',
             '@fortawesome/fontawesome-svg-core': '@fortawesome/fontawesome-svg-core',
             '@fortawesome/free-solid-svg-icons': '@fortawesome/free-solid-svg-icons',
             '@fortawesome/react-fontawesome': '@fortawesome/react-fontawesome'
