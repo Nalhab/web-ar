3428317f4b257447fd8aae5e6f4f5bdbcd21cb6d
Nalhab <nathanlrbn@gmail.com>
2024-11-16 19:02:21 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
-import { externalizeDeps } from 'vite-plugin-externalize-deps';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/three_vite_xr",
+    base: "/",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
             supported: {
                 'top-level-await': true
             }
-        }
+        },
+        include: ['react', 'react-dom']
     },
     esbuild: {
         supported: {
@@ -20,11 +21,22 @@ export default defineConfig({
     },
     build: {
         sourcemap: true,
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react/jsx-runtime': 'jsxRuntime'
+                }
+            }
+        }
     },
     server: {
         open: true
     },
     plugins: [
+        react(),
         viteStaticCopy({
             targets: [
                 { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
@@ -35,9 +47,6 @@ export default defineConfig({
                 { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
             ]
         }),
-        glsl(),
-        externalizeDeps({
-            include: ['react', 'react-dom']
-        })
+        glsl()
     ]
 })
